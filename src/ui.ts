c13ff134d7a7cdbbf659e428e130099ae1541fea
hiloki <service@inkdesign.jp>
2020-06-25 18:02:50 +0900

    format switcher

--- src/ui.ts
+++ src/ui.ts
@@ -1,20 +1,53 @@
 import './ui.css'
 import { json, css, scss } from './template';
 
-parent.postMessage({ pluginMessage: { type: 'collect-tokens' } }, '*')
+parent.postMessage({ pluginMessage: { type: 'collect-tokens' } }, '*');
 
 onmessage = message => {
   if (message.data.pluginMessage.type == "collect-tokens") {
 
-    // TODO: nav操作で切り替え
     // TODO: コピーボタン
     // TODO: HEXとRGBA
-    
-    let params = "";
+    // TODO: prefixの付与
+    // TODO: カスタムテンプレート
 
-    const element: HTMLInputElement =<HTMLInputElement>document.getElementById('output');
-    const value: string = element.value;
+    const buttons = document.querySelectorAll('#format > button');
+    const output: HTMLInputElement =<HTMLInputElement>document.getElementById('output');
+    const data = message.data.pluginMessage.data;
 
-    element.value = scss(message.data.pluginMessage.data);
+    const switchFormat = (format) => {
+      switch (format) {
+        case 'json':
+          output.value = json(data)
+          break;
+        case 'css':
+          output.value = css(data)
+          break;
+        case 'scss':
+          output.value = scss(data)
+          break;
+        default:
+          output.value = json(data)
+      }
+    }
+
+    const switchFormatHandler = (e) => {
+      const activeClass = 'active';
+      const current = Array.from(buttons).find((element) => {
+        return element.classList.contains(activeClass);
+      });
+      const selected = e.target;
+
+      switchFormat(selected.dataset.format);
+
+      current.classList.remove(activeClass);
+      selected.classList.add(activeClass);
+    }
+
+    buttons.forEach((button) => {
+      button.addEventListener('click', switchFormatHandler)
+    })
+
+    switchFormat('json');
   }
 };
